aab4f457187179001c8d0aa9c5a6aa4a4374478c
Leonardo Leal <91156801+Leohgb@users.noreply.github.com>
2023-09-27 10:20:22 -0300

    Create transcricao.model.js

---
+++ Backend/app/models/transcricao.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+var d = new Date();
+
+const Transcricao = new Schema({
+    id_transcricao:{
+        type: Number,
+        required: true
+    },
+    nome_musica: {
+        type: String, 
+        required: true,
+    },
+    nome_versao:{
+        type:String,
+        required:true
+    },
+    data_criacao:{
+        type: String,
+        default: d.toLocaleDateString()
+    },
+    genero:{
+        type: String,
+        required: true
+    },
+    arquivo_pdf:{
+        id: Number,
+        nome:String,
+        extensao: String,
+        required: true
+    },
+    tipo:{
+        type: String,
+        required: true
+    },
+    dificuldade:{
+        type: String,
+        required: true
+    },
+    instrumento:{
+        type: String,
+        required: true
+    }
+})
+
+const transcricao = mongoose.model("transcricao", Transcricao)
+
