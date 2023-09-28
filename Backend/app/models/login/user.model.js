b10d1c220a6567891052f482cbdf6ff410a10f2f
Leohgb <leonardokgb12@gmail.com>
2023-09-28 10:27:23 -0300

    Model export atualizado

--- Backend/app/models/login/user.model.js
+++ Backend/app/models/login/user.model.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    roles: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role"
-      }
-    ]
-  })
-);
+const Usuario = new Schema({
+  username: String,
+  email: String,
+  password: String,
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role"
+    }
+  ]
+})
 
-module.exports = User;
+export const User = mongoose.model("User", Usuario);
