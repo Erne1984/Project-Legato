69eb9ea1453bc2715d634794fd70ca3503a827a9
Leohgb <leonardokgb12@gmail.com>
2023-10-03 16:24:07 -0300

    Home

--- Frontend/project_legato/src/pages/Home/Home.jsx
+++ Frontend/project_legato/src/pages/Home/Home.jsx
@@ -11,9 +11,20 @@ const Home = () => {
         <main className="home-container">
             <Filters />
             <div className="content">
-            <h2>Músicas mais acessadas</h2>
-            <TableMusics></TableMusics>
-            <h2>Artistas mais acessados</h2>
+                <div className="title-header">
+                    <h2>Músicas mais acessadas</h2>
+                    <a href=""><h5>Ver top</h5></a>
+                </div>
+                <TableMusics></TableMusics>
+                <div className="title-header">
+                    <h2>Artistas mais acessados</h2>
+                    <a href=""><h5>Ver top</h5></a>
+                </div>
+                <div className="title-header">
+                    <h2>Conheça nossas comunidades</h2>
+                    <a href=""><h5>Ir para comunidades</h5></a>
+                </div>
+
             </div>
         </main>
     )
