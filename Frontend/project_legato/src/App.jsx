b5d57a17c636fc1b9b8495e0c1c4db0184cb26d1
ernesto <ernestofamorim@gmail.com>
2023-10-04 01:15:53 -0300

    footer

--- Frontend/project_legato/src/App.jsx
+++ Frontend/project_legato/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import Explorar from './pages/Explorar/Explorar';
 
@@ -16,6 +17,7 @@ function App() {
       <Route path="/Explorar" element={ <Explorar/> } />
 
     </Routes>
+    <Footer/>
   </Router>
 
   );
