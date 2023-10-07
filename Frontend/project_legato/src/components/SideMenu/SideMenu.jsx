a928bc3f62af696c4e429517a738f4016a5047e9
ernesto <ernestofamorim@gmail.com>
2023-10-07 04:23:28 -0300

    SideMenu

--- Frontend/project_legato/src/components/SideMenu/SideMenu.jsx
+++ Frontend/project_legato/src/components/SideMenu/SideMenu.jsx
@@ -6,6 +6,12 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
 import DownloadIcon from '@mui/icons-material/Download';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AppsIcon from '@mui/icons-material/Apps';
+import Afinacao from '../../assets/afinacao.png';
+
+
 
 const SideMenu = () => {
     return (
@@ -15,14 +21,14 @@ const SideMenu = () => {
                 <Avatar avatarUrl={"https://www.nme.com/wp-content/uploads/2018/10/BASEMENT-7-Mitchell-Wojcik.jpg"} />
 
                 <ul className="actions-buttons">
-                    <li className="btn-li"><span className="btn-side"><UnfoldMoreIcon/> Rolagem</span></li>
-                    <li className="btn-li"><span className="btn-side"><RemoveIcon/> Texto <AddIcon/></span></li>
-                    <li className="btn-li"><span className="btn-side"><RemoveIcon/>  Tom <AddIcon/></span></li>
-                    <li><span><DownloadIcon/> Baixar </span></li>
-                    <li><span>Corrigir</span></li>
-                    <li><span>Afinação</span></li>
-                    <li><span>Favoritar</span></li>
-                    <li><span>Exibir</span></li>
+                    <li className="btn-li"><span className="btn-side"><UnfoldMoreIcon /> Rolagem</span></li>
+                    <li className="btn-li"><span className="btn-side"><RemoveIcon /> Texto <AddIcon /></span></li>
+                    <li className="btn-li"><span className="btn-side"><RemoveIcon />  Tom <AddIcon /></span></li>
+                    <li><span className="element-li" ><DownloadIcon /> <span>Baixar</span> </span></li>
+                    <li ><span className="element-li"><EditIcon /> <span>Corrigir</span></span></li>
+                    <li ><span className="element-li"><img src={Afinacao} width="24px" /> <span>Afinação</span></span></li>
+                    <li ><span className="element-li"><FavoriteBorderIcon /> <span>Favoritar</span></span></li>
+                    <li ><span className="element-li"><AppsIcon/> <span >Exibir</span></span></li>
                 </ul>
             </div>
 
