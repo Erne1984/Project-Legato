c58eac218e1f4df9ae71e56b1f09a38ac5c66278
ernesto <ernestofamorim@gmail.com>
2023-10-08 09:58:14 -0300

    header perfil-usuario

--- Frontend/project_legato/src/pages/Perfil_Usuario/Perfil_Usuario.jsx
+++ Frontend/project_legato/src/pages/Perfil_Usuario/Perfil_Usuario.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './Perfil_Usuario.css';
+
 import Perfil from '../../components/Perfil/Perfil';
+import AddIcon from '@mui/icons-material/Add';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 
-const Perfil_Usuario = () => {
+const Perfil_Usuario = ({ }) => {
     return (
         <div className='Perfil_usuario'>
             <Perfil />
             <main className='container-usuario'>
-                <h2>Pagina</h2>
+                <div className='header-perfil'>
+
+                    <div className='titulo-perfil-usuario'>
+                        <h2 >Bio</h2>
+                        <button className='btn-bio'><AddIcon /><span>Editar Bio</span></button>
+                    </div>
+
+
+                    <div className='box-seguidores'>
+                        <span className='seguidores'>0 Seguidores</span>
+                        <span className='seguindo'>0 Seguindo</span>
+                        <SettingsIcon/>
+                    </div>
+
+                </div>
+
             </main>
         </div>
     )
