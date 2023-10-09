388d4852dd0aef244c433208bedfee4559882742
Leohgb <leonardokgb12@gmail.com>
2023-10-08 23:14:30 -0300

    Dark_mode

--- Frontend/project_legato/src/GlobalStyle.jsx
+++ Frontend/project_legato/src/GlobalStyle.jsx
@@ -1,7 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    body {
+    body{
+       background-color: ${(props) => props.theme.body};
+       color: ${(props) => props.theme.fontColor};
+    }
+
+    h1, h2, h3, td, li{
+        color: ${(props) => props.theme.fontColor};
+    }
+
+    .home-container, .container-header {
         background-color: ${(props) => props.theme.body};
     }
 `;
